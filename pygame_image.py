a9444ce632f8d0c4bd4895cb31320e31662ce0c7
C0A23172 <c0a23172be@edu.teu.ac.jp>
2024-06-11 16:20:49 +0900

    練習6間延びを防ぐ

--- pygame_image.py
+++ pygame_image.py
@@ -17,11 +17,12 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+        x = tmr%800
+        screen.blit(bg_img, [-x, 0])
         kk_rct = kk_img.get_rect() #画像Surfaceに対応する画像Rectを取得する
         kk_rct.center = 300, 200 #中心座標を300, 200に設定する
         screen.blit(kk_img, kk_rct) #画像SurfaceをスクリーンSurfaceにRectに従って貼り付ける
+        
         pg.display.update()
         tmr += 1        
         clock.tick(200)
